d1b969a34e7cfa914bc217c30644f5f686a1f7e5
suraj05 <wsurajml007@gmail.com>
2026-01-02 11:22:42 +0000

    Enhance :  server info script with interactive menu

--- system-info/server-info.sh
+++ system-info/server-info.sh
@@ -30,7 +30,7 @@ function tcp_check(){
 }
 
 #Check the exact Kernel version
-function kernal_check(){
+function kernel_check(){
     echo ""
     echo "Kernel version on ${server_name} is : $(uname -r)"
     echo ""
@@ -40,7 +40,51 @@ function all_check(){
     memory_check
     cpu_check
     tcp_check
-    kernal_check
+    kernel_check
 }
 
-all_check
+##
+# color variables
+##
+green='\e[32m'
+blue='\e[34m'
+red='\e[31m'
+clear='\e[0m'
+
+##
+# color functions
+##
+
+function color_green(){
+    echo -ne ${green}${1}${clear}
+}
+
+function color_blue(){
+    echo -ne ${blue}${1}${clear}
+}
+
+function menu(){
+    echo -ne "
+    My First Menu
+    $(color_green '1)') Memory usage
+    $(color_green '2)') CPU load
+    $(color_green '3)') Number of TCP connections 
+    $(color_green '4)') Kernel version
+    $(color_green '5)') Check All
+    $(color_green '0)') Exit
+    $(color_blue 'Choose an option:') "
+    read a
+
+    case $a in
+        0) exit 0 ;;
+        1) memory_check ; menu ;;
+        2) cpu_check ; menu ;;
+        3) tcp_check ; menu ;;
+        4) kernel_check ; menu ;;
+        5) all_check ; menu ;;
+        *) echo -e "${red}Wrong option.${clear}"; menu ;;
+    esac
+}
+
+# Call the menu function
+menu
